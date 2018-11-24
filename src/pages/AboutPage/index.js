6941e9e6a179dc4b520792fc56d35fe1402b17a1
marlbones <mdimaanods@gmail.com>
2018-11-25 00:26:12 +1100

    Copy change

--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -11,7 +11,7 @@ const AboutPage = ({ classes }) => (
       <div className={classes.info}>
         <p className = {classes.sub}>Hi there.</p>
         <p className={classes.sub}>My name's Marlon and I'm an Australian born freelance web developer. I'm currently based in Ghana, but accept work from anywhere in the world. I hate the term "digital nomad", but I suppose that's what I am right now. Please don't judge me.</p>
-        <p className={classes.sub}>Having previously worked as a social worker for about 7 years, I finally gave in to the call of the tech world and changed careers.</p>
+        <p className={classes.sub}>Having worked as a social worker for about 7 years, I finally caved to the call of the tech world and changed careers.</p>
         <p className={classes.sub}>My current stack of choice is <strong>Html</strong>, <strong>CSS</strong>, <strong>Javascript</strong>, <strong>React/React Native</strong>, <strong>Node.js</strong>, <strong>Apollo</strong> and <strong>GraphQL</strong>; however I love experimenting and learning new things.</p>
         <p className={classes.sub}>I've worked as a team member at <a href="https://ngny.com.au/">Ngakkan Nyaagu</a> and <a href="https://trioxis.com/">Trioxis</a>, but have also made signficant contributions as a freelancer to products by <a href="https://www.styleatlas.co/">Style Atlas</a> and <a href="http://cliniknote.com/">Clinik Note</a>.</p>
         <p className={classes.sub}>Thanks for stopping by.</p>
